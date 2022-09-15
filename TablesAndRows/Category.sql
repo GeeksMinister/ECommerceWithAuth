a833ab9b8f84c879b685c12bddd2a7e11ce0c5c8
GeeksMinister <98697297+GeeksMinister@users.noreply.github.com>
2022-09-15 20:26:52 +0200

    Create Category.sql

---
+++ TablesAndRows/Category.sql
@@ -0,0 +1,6 @@
+insert into Category (Guid, Name) values ('4256C7B9-C5BA-4C07-8C3E-49326AE4655F', 'Meat');
+insert into Category (Guid, Name) values ('DC9E248E-F1A8-4BE4-85B7-85905FC435FC', 'Dried Goods');
+insert into Category (Guid, Name) values ('10CB0559-5F64-4B37-9313-6CECBE7A22FF', 'Bread');
+insert into Category (Guid, Name) values ('DD631C62-A58D-4675-B314-11BBE3324920', 'Vegetables & Fruit');
+insert into Category (Guid, Name) values ('F3D88DF2-8C24-4138-A9E2-1ADE9D7A89E2', 'Dairy');
+insert into Category (Guid, Name) values ('7D71425B-64BA-4657-9ECE-399B113FAE4C', 'Liquids');
